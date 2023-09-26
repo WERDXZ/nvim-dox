3fb535a2689ac25a8ba492e0c727de684a4a19a2
WERDXZ <lwerdxzl@hotmail.com>
2023-09-26 13:46:20 -0700

    changed test file

--- test/test.cpp
+++ test/test.cpp
@@ -4,10 +4,22 @@ int test(){
 	return 0;
 }
 
-using namespace std;
-
-int main(){
-	cout << "Hello World!" << endl;
-	return test();
+template<typename T, typename M>
+decltype(auto) getT(T t, M m){
+	m+t;
+	return t;
 }
 
+int add(int a, int b){
+	return a+b;
+}
+
+class T{};
+
+struct M{};
+
+template<typename T>
+class t{};
+
+template<typename M>
+struct m{};
